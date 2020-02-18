4e19c28069d7966c269552efbe2b55c0515d844b
thenatural86 <philip.smalls86@gmail.com>
2020-02-18 16:25:50 -0500

    refs

---
+++ src/components/ImageCard.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+class ImageCard extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.imageRef = React.createRef()
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans)
+  }
+
+  setSpans = () => {
+    console.log(this.imageRef.current.clientHeight)
+  }
+
+  render() {
+    const { description, urls } = this.props.image
+    return (
+      <div>
+        <img src={urls.regular} alt={description} ref={this.imageRef} />
+      </div>
+    )
+  }
+}
+
+export default ImageCard
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "./ImageList.css"
+import ImageCard from "./ImageCard"
 
 const ImageList = props => {
-  const images = props.images.map(({ description, id, urls }) => {
-    return <img src={urls.regular} key={id} alt={description} />
+  const images = props.images.map(image => {
+    return <ImageCard image={image} key={image.id} />
   })
   return <div className="image-list">{images}</div>
 }
