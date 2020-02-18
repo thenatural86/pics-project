a8451fd873e2eb5235c8e5f986b310715cf115c4
thenatural86 <philip.smalls86@gmail.com>
2020-02-18 11:58:45 -0500

    async/await

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,14 @@ import axios from "axios"
 import SearchBar from "./SearchBar"
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    axios.get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID E3MgrtDYzwNDsc6_CXeLkAosa9XPjrti8xZJ-sO_7-U"
       }
     })
+    console.log(response.data.results)
   }
 
   render() {
